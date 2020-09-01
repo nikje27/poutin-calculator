0a54ce5ede185b14b70d5e4fe7b8e4055c51267e
JaninaRenkes <renkesjanina@gmail.com>
2020-09-01 11:36:02 +0200

    add test

---
+++ test_main.py
@@ -0,0 +1,7 @@
+import unittest
+import main.py
+
+def test_substraction(self):
+    self.assertEqual(calc.substract(10, 5), 5)
+    self.assertEqual(calc.substract(-1, 1), -2)
+    self.assertEqual(calc.substract(-1, -1), 0)
