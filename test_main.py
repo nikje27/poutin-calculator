910bc8c01b43691878e3a6facb082c26e011a25c
Alessandro Gallina <alessandrogallina1992@gmail.com>
2020-09-01 12:26:33 +0200

    Update test_main.py

---
+++ test_main.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+from main import divide, add, multiply, subtract
+
+class TestCalc(TestCase):
+
+    def test_divide(self):
+        self.assertEqual(divide(10, 5), 2)
+        self.assertEqual(divide(-1, 1), -1)
+        self.assertEqual(divide(-1, -1), 1)
+        self.assertEqual(divide(5, 2), 2.5)
+
+        with self.assertRaises(ValueError):
+            divide(10, 0)
+            
+    def test_subtract(self):
+        self.assertEqual(subtract(10, 5), 5)
+        self.assertEqual(subtract(-1, 1), -2)
+        self.assertEqual(subtract(-1, -1), 0)
+      
+    def test_multiply(self):
+        self.assertEqual(multiply(10, 5), 50)
+        self.assertEqual(multiply(-1, 1), -1)
+        self.assertEqual(multiply(-1, -1), 1)
+
+    def test_add(self):
+        self.assertEqual(add(10, 5), 15)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
+        
+        
