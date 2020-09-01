09675c9a04bf47447220bf71deef1db688c3027a
nikje27 <66448178+nikje27@users.noreply.github.com>
2020-09-01 11:36:20 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,12 +1,12 @@
 import unittest
-import calc
+from main import multiply
 
 class TestCalc(unittest.TestCase):
 
   def test_multiply(self):
-          self.assertEqual(calc.multiply(10, 5), 50)
-          self.assertEqual(calc.multiply(-1, 1), -1)
-          self.assertEqual(calc.multiply(-1, -1), 1)
+          self.assertEqual(main.multiply(10, 5), 50)
+          self.assertEqual(main.multiply(-1, 1), -1)
+          self.assertEqual(main.multiply(-1, -1), 1)
 
 if __name__ == '__main__':
     unittest.main()
