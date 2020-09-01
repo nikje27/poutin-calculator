9c703850fcd561d2ddabe081590cc78c257ced22
alligalli <34606119+alligalli@users.noreply.github.com>
2020-09-01 11:17:42 +0200

    Update test_main.py
    corrected functions call syntax in test_main.py file

--- test_main.py
+++ test_main.py
@@ -4,11 +4,11 @@ from main import divide
 class TestCalc(TestCase):
 
     def test_divide(self):
-        self.assertEqual(calc.divide(10, 5), 2)
-        self.assertEqual(calc.divide(-1, 1), -1)
-        self.assertEqual(calc.divide(-1, -1), 1)
-        self.assertEqual(calc.divide(5, 2), 2.5)
+        self.assertEqual(divide(10, 5), 2)
+        self.assertEqual(divide(-1, 1), -1)
+        self.assertEqual(divide(-1, -1), 1)
+        self.assertEqual(divide(5, 2), 2.5)
 
         with self.assertRaises(ValueError):
-            calc.divide(10, 0)
+            divide(10, 0)
 
