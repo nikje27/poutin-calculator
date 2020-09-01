c5a6bf85417937c7b8ed5f66d5bf0e34e6b58a0d
JaninaRenkes <renkesjanina@gmail.com>
2020-09-01 11:41:47 +0200

    test change

--- test_main.py
+++ test_main.py
@@ -2,6 +2,6 @@ import unittest
 import main.py
 
 def test_substraction(self):
-    self.assertEqual(calc.substract(10, 5), 5)
-    self.assertEqual(calc.substract(-1, 1), -2)
-    self.assertEqual(calc.substract(-1, -1), 0)
+    self.assertEqual(main.substract(10, 5), 5)
+    self.assertEqual(main.substract(-1, 1), -2)
+    self.assertEqual(main.substract(-1, -1), 0)
